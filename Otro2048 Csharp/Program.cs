fb1820dfb3ed8fd3cf1d4005f55cc12ec3f9f8bb
Federico Muntuny <FMUNTUNY@GMAIL.COM>
2022-05-14 17:04:15 -0300

    Revert "Revert "Primer Comit, tablero funcionando""
    This reverts commit 582f35094879e90c7efff3627c494c05694ea356.

--- Otro2048 Csharp/Program.cs
+++ Otro2048 Csharp/Program.cs
@@ -1,3 +1,5 @@
+using Otro2048_Csharp.clases;
+
 namespace Otro2048_Csharp
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Otro2048_Csharp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FrmTablero());
 
         }
     }
