c2aaec6bfb43f66d69bdbd13892689dea673cf78
Federico Muntuny <FMUNTUNY@GMAIL.COM>
2022-05-14 14:19:20 -0300

    Agregar archivos de proyecto.

---
+++ Otro2048 Csharp/Form1.cs
@@ -0,0 +1,12 @@
+using Otro2048_Csharp.clases;
+
+namespace Otro2048_Csharp
+{
+    public partial class Tablero : Form
+    {
+        public Tablero()
+        {
+            Cuadrado cuadrado = new Cuadrado();
+        }
+    }
+}
