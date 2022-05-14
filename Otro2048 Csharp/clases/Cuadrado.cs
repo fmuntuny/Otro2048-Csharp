582f35094879e90c7efff3627c494c05694ea356
Federico Muntuny <FMUNTUNY@GMAIL.COM>
2022-05-14 17:01:17 -0300

    Revert "Primer Comit, tablero funcionando"
    This reverts commit eeea5d965d70fb4bdeb40dd8e3966f36f6a83a70.

--- Otro2048 Csharp/clases/Cuadrado.cs
+++ Otro2048 Csharp/clases/Cuadrado.cs
@@ -6,89 +6,113 @@ using System.Threading.Tasks;
 
 namespace Otro2048_Csharp.clases
 {
-
-    internal class Cuadrado : Label
+    
+internal class Cuadrado : Label
     {
         private int valor;
-
+        private Color colorFondo;
+        /*
+         */
         public Cuadrado(int val)
         {
-            this.setValor(val);
+            this.valor = val;
+            this.Text = this.valor.ToString();
             this.Font = new Font("Papyrus", 30, FontStyle.Bold);
             this.BorderStyle = BorderStyle.Fixed3D;
             this.TextAlign = ContentAlignment.MiddleCenter;
-            this.Size = new Size(90, 90);
+            this.setColorFondo();
+            this.Size = new Size(90,90);
             this.Visible = true;
         }
 
         public Cuadrado()
         {
-            this.setValor(0);
+            this.valor = val;
+            this.Text = this.valor.ToString();
             this.Font = new Font("Papyrus", 30, FontStyle.Bold);
             this.BorderStyle = BorderStyle.Fixed3D;
             this.TextAlign = ContentAlignment.MiddleCenter;
+            this.setColorFondo();
             this.Size = new Size(90, 90);
             this.Visible = true;
         }
 
 
-        public void setValor(int valor)
+        public void setValor(int valor) 
         {
             this.valor = valor;
-            this.Text = this.valor.ToString();
-            this.setColorFondo();
         }
         public int getValor()
         {
             return this.valor;
         }
-        public Color getColorFondo()
+        public Color getColorFondo() 
         {
             return this.BackColor;
         }
-        public void setColorFondo()
+        public void setColorFondo() 
         {
-            switch (this.getValor())
+                switch (this.getValor())
+                {
+                    case 0:
+                        this.BackColor = Color.FromArgb(224, 224, 224);
+                        break;
+                    case 2:
+                        this.BackColor = Color.FromArgb(224, 224, 224);
+                        break;
+                    case 4:
+                        this.BackColor = Color.Silver;
+                        break;
+                    case 8:
+                        this.BackColor = Color.FromArgb(255, 255, 192);
+                        break;
+                    case 16:
+                        this.BackColor = Color.Yellow;
+                        break;
+                    case 32:
+                        this.BackColor = Color.FromArgb(255, 128, 128);
+                        break;
+                    case 64:
+                        this.BackColor = Color.Red;
+                        break;
+                    case 128:
+                        this.BackColor = Color.Red;
+                        break;
+                    case 256:
+                        this.BackColor = Color.Red;
+                        break;
+                    case 512:
+                        this.BackColor = Color.FromArgb(192, 192, 255);
+                        break;
+                    case 1024:
+                        this.BackColor = Color.FromArgb(192, 192, 255);
+                        break;
+                    case 2048:
+                        this.BackColor = Color.Blue;
+                        break;
+                    default:
+                        break;
+                }
+            
+        }
+
+        public void dibujarTablero() 
+        {
+            int posX = 0;
+            int posY = 0;
+            for (int i = 0; i < 4; i++)
             {
-                case 0:
-                    this.BackColor = Color.FromArgb(224, 224, 224);
-                    break;
-                case 2:
-                    this.BackColor = Color.FromArgb(224, 224, 224);
-                    break;
-                case 4:
-                    this.BackColor = Color.Silver;
-                    break;
-                case 8:
-                    this.BackColor = Color.FromArgb(255, 255, 192);
-                    break;
-                case 16:
-                    this.BackColor = Color.Yellow;
-                    break;
-                case 32:
-                    this.BackColor = Color.FromArgb(255, 128, 128);
-                    break;
-                case 64:
-                    this.BackColor = Color.Red;
-                    break;
-                case 128:
-                    this.BackColor = Color.Red;
-                    break;
-                case 256:
-                    this.BackColor = Color.Red;
-                    break;
-                case 512:
-                    this.BackColor = Color.FromArgb(192, 192, 255);
-                    break;
-                case 1024:
-                    this.BackColor = Color.FromArgb(192, 192, 255);
-                    break;
-                case 2048:
-                    this.BackColor = Color.Blue;
-                    break;
-                default:
-                    break;
+                for (int j = 0; j < 4; j++)
+                {
+                    Cuadrado cuadrado = new Cuadrado(0);
+                    this.Controls.Add(cuadrado);
+                    cuadrado.Location = new Point(posX, posY);
+                    posX += 93;
+                }
+                posY += 93;
+                posX = 0;
             }
         }
+
     }
 }
